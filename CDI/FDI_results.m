4911f9282daa181829ddecfdebbc29931084f604
MarinosCh <mchris03@ucy.ac.cy>
2013-07-05 13:43:30 +0300

    Update FDI_results.m

--- CDI/FDI_results.m
+++ CDI/FDI_results.m
@@ -539,3 +539,4 @@ function edit2_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
+
