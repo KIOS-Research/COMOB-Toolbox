5394929b45ffb1874a9e32c7ca2a1a2a167c9a9c
Alexis Kyriacou <akyria09@ucy.ac.cy>
2020-10-23 11:26:08 +0300

    MATLAB Compatibility Update
    Change Struct name (i.e., arguments) to "argumentsC" since in versions 2019a and later "arguments" is a reserved word

--- MatlabContamToolbox.m
+++ MatlabContamToolbox.m
@@ -1,7 +1,7 @@
 %{
  Copyright (c) 2018 KIOS Research and Innovation Centre of Excellence
  (KIOS CoE), University of Cyprus (www.kios.org.cy)
- 
+  
  Licensed under the EUPL, Version 1.1 or – as soon they will be approved 
  by the European Commission - subsequent versions of the EUPL (the "Licence");
  You may not use this work except in compliance with theLicence.
